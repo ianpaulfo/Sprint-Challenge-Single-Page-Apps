a86ae7298c1604fad2470b6d8d4278176cddb20b
Ian Fragoso <IanPaul@Ians-MacBook-Pro.local>
2019-12-13 21:27:04 -0700

    added search form functionality

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Axios  from "axios";
+import CharacterCard from "./CharacterCard";
+import CharacterList from "./CharacterList";
+
+
 
 export default function SearchForm() {
+
+  const [name, setName] = useState("");
+  const [search, setSearch] = useState([]);
+
+  function onSearch(name) {
+    Axios
+    .get(`https://rickandmortyapi.com/api/character/`)
+    .then(response => {
+      setSearch(response.data.results);
+      console.log("search", response.data.results);
+    }
+    );
+  }
+
+  const handleInputChange = event => {
+    setName(event.target.value);
+  };
  
   return (
     <section className="search-form">
-     // Add a search form here
+     <form onSubmit={event => {
+        event.preventDefault();
+        onSearch(name);
+      }}
+      >
+        <input onChange={event => handleInputChange(event)}
+        placeholder="name"
+        value={name}
+        name="name"/>
+        <button type="submit">Search</button>
+      </form>
+
+      {search.map(search => {
+        return (
+          <div className="searchResults">
+            <img src={search.image}/>
+            <h2 className="names">{search.name}</h2>
+            <p>{search.species}</p>
+          </div>
+        )
+      })}
+      
+
     </section>
   );
 }
